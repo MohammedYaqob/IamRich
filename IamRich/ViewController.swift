6c309dda459d6f44284478f93d3f140862deed5d
Mohammed Yaqob <mohammed.yaqob.98@gmail.com>
2021-08-28 10:36:42 +0200

    Initial Commit

---
+++ IamRich/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  IamRich
+//
+//  Created by Mohammed Yaqob's MacBook on 23/07/2020.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
